3416469d7ce390d3ca028254a4be8c534fbe5aaa
Tearth <tearthdev@gmail.com>
2021-02-08 22:41:04 +0100

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,8 +1,65 @@
 $(document).ready(function() {
+    generateLayout('#layout1', 0);
+    generateLayout('#layout2', 1);
+    generateLayout('#layout3', 2);
+    generateLayout('#layout4', 3);
+    
     generateBitboard('#bitboard1', false);
     generateBitboard('#bitboard2', false);
     generateBitboard('#bitboard3', true);
 });
+
+function generateLayout(areaId, variant) {
+    var area = $(areaId);
+    
+    var radioRow = $(document.createElement('div')).prop({
+        class: 'radio-row'
+    });
+    
+    var radio = $(document.createElement('input')).prop({
+        type: 'radio',
+        name: 'radioLayout',
+        class: 'form-check-input',
+        value: variant
+    });
+    
+    var radioLabel = $(document.createElement('label')).prop({
+        class: 'form-check-label'
+    }).html('test');
+    
+    radioRow.append(radio);
+    radioRow.append(radioLabel);
+    area.append(radioRow);
+    
+    for (var y = 0; y < 8; y++) {
+        var row = $(document.createElement('div')).prop({
+            class: 'layout-row'
+        });
+        
+        for (var x = 0; x < 8; x++) {
+            var value = getLayoutVariant(variant, x, y);
+            if (value < 10) {
+                value = "0" + value;
+            }
+            
+            var span = $(document.createElement('span')).html(value);
+            row.append(span);
+        }
+        
+        area.append(row);
+    }
+}
+
+function getLayoutVariant(variant, x, y) {
+    switch (variant) {
+        case 0: return 63 - (x + y * 8);
+        case 1: return 63 - (7 - x + y * 8);
+        case 2: return x + y * 8;
+        case 3: return 7 - x + y * 8;
+    }
+    
+    return 0;
+}
 
 function generateBitboard(areaId, readOnly) {
     var area = $(areaId);
@@ -26,57 +83,4 @@ function generateBitboard(areaId, readOnly) {
         
         area.append(row);
     }
-    
-    
-    if (readOnly) {
-        area.append(generateGroupOfButtons(true));
-    } else {
-        area.append(generateGroupOfButtons(false));
-        area.append(generateTextbox('Decimal'));
-        area.append(generateTextbox('Hexadecimal'));
-        area.append(generateTextbox('Binary'));
-    }
-}
-
-function generateTextbox(label) {
-    var div = $(document.createElement('div')).prop({
-        class: 'input-row'
-    });
-    
-    var textbox = $(document.createElement('input')).prop({
-        type: 'textbox',
-        class: 'form-control',
-        placeholder: label
-    });
-    
-    div.append(textbox);
-    return div;
-}
-
-function generateGroupOfButtons(readOnly) {
-    var group = $(document.createElement('div')).prop({
-        class: 'btn-group buttons-row'
-    });
-    
-    if (readOnly) {
-        group.append(generateButton('AND'));
-        group.append(generateButton('OR'));
-        group.append(generateButton('XOR'));
-        group.append(generateButton('NOT'));
-    } else {
-        group.append(generateButton('Fill'));
-        group.append(generateButton('Clear'));
-        group.append(generateButton('Shift left'));
-        group.append(generateButton('Shift right'));
-    }
-    
-    return group;
-}
-
-function generateButton(value) {
-    return $(document.createElement('input')).prop({
-        type: 'button',
-        class: 'btn btn-info',
-        value: value
-    });
 }
