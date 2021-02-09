ea3f7c138827fd4782ab958c129f9e75f7792c9d
Tearth <tearthdev@gmail.com>
2021-02-09 21:14:33 +0100

    Add logic for textboxes

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ $(document).ready(function() {
     generateBitboard('#bitboard1', false);
     generateBitboard('#bitboard2', false);
     generateBitboard('#bitboard3', true);
+    
+    $('#decBitboard1').keyup(decBitboard1KeyUp);
+    $('#hexBitboard1').keyup(hexBitboard1KeyUp);
+    $('#binBitboard1').keyup(binBitboard1KeyUp);
+    
+    $('#decBitboard2').keyup(decBitboard2KeyUp);
+    $('#hexBitboard2').keyup(hexBitboard2KeyUp);
+    $('#binBitboard2').keyup(binBitboard2KeyUp);
 });
 
 function generateLayout(areaId, variant) {
@@ -19,7 +27,7 @@ function generateLayout(areaId, variant) {
         for (var x = 0; x < 8; x++) {
             var value = getLayoutVariant(variant, x, y);
             if (value < 10) {
-                value = "0" + value;
+                value = '0' + value;
             }
             
             var span = $(document.createElement('span')).html(value);
@@ -63,4 +71,58 @@ function generateBitboard(areaId, readOnly) {
         
         area.append(row);
     }
+}
+
+function decBitboard1KeyUp() {
+    decKeyUp($('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'));
+}
+
+function hexBitboard1KeyUp() {
+    hexKeyUp($('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'));
+}
+
+function binBitboard1KeyUp() {
+    binKeyUp($('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'));
+}
+
+function decBitboard2KeyUp() {
+    decKeyUp($('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'));
+}
+
+function hexBitboard2KeyUp() {
+    hexKeyUp($('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'));
+}
+
+function binBitboard2KeyUp() {
+    binKeyUp($('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'));
+}
+
+function andBitboard3Click() {
+    
+}
+
+function orBitboard3Click() {
+    
+}
+
+function xorBitboard3Click() {
+    
+}
+
+function decKeyUp(decTextbox, hexTextbox, binTextbox) {
+    var value = parseInt(decTextbox.val(), 10);
+    hexTextbox.val('0x' + value.toString(16));
+    binTextbox.val(value.toString(2));
+}
+
+function hexKeyUp(decTextbox, hexTextbox, binTextbox) {
+    var value = parseInt(hexTextbox.val(), 16);
+    decTextbox.val(value.toString(10));
+    binTextbox.val(value.toString(2));
+}
+
+function binKeyUp(decTextbox, hexTextbox, binTextbox) {
+    var value = parseInt(binTextbox.val(), 2);
+    decTextbox.val(value.toString(10));
+    hexTextbox.val('0x' + value.toString(16));
 }
